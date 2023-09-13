ea0d192c9e09fa6a660fac01e1c96907a3d653f9
smirniy <89267095924@mail.ru>
2023-09-13 10:55:51 +0300

    djankoy_3

--- inside/views.py
+++ inside/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 from .models import Bd
 
 
 def index(request):
-    s = 'Объявления \r\n\r\n'
-    for bb in Bd.objects.all():
-        s += bb.title +'\r\n' + bb.content + '\r\n\r\n'
-    return HttpResponse(s, content_type='text/plain', charset='utf-8')
+    bbs = Bd.objects.order_by('-published')
+    return render(request, 'inside/index.html', {'bbs':bbs})
+
