035413ed5f1f34427aff82946d556e1f8812c08e
smirniy <89267095924@mail.ru>
2023-09-19 22:57:11 +0300

    djankoy_5

--- inside/admin.py
+++ inside/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Bd
+from .models import Bd, Rubric
 
 
 # Register your models here.
@@ -10,3 +10,4 @@ class BdAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Bd, BdAdmin)
+admin.site.register(Rubric)
